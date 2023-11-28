4bf688c14a4d74920e559946d52b08b9dd7deb0c
takikamaru <58665878+takikamaru@users.noreply.github.com>
2023-11-28 22:17:23 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# test_2
+# HTML &amp; CSS практика
+Шаблон для HTML &amp; CSS практики на YouTube канале AVIS TV
---
+++ index.js
@@ -0,0 +1,5 @@
+function draw() {
+  var ctx = document.getElementById('canvas').getContext('2d');
+  ctx.font = "48px serif";
+  ctx.strokeText("Barotrauma", 10, 50);
+}
